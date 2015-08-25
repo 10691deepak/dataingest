cde2104765b7da862a2f800d44cb841568289e30
Blake Boswell <j.blake.boswell@gmail.com>
2015-08-24 21:27:39 -0400

    first method

---
+++ main.R
@@ -0,0 +1,55 @@
+
+## ------------------------------------------------------------------
+## ---- load packages
+## ------------------------------------------------------------------
+package.manager <- function(pkgs){
+  loaded <- sapply(pkgs, function(pkg){
+    if(require(pkg, character.only = TRUE)){
+      TRUE
+    }else
+    {
+      install.packages(pkg)
+      require(pkg, character.only = TRUE)
+    }
+  })
+  
+  print('Package Util Require Result:')
+  print(data.frame(Loaded = loaded, row.names = pkgs))
+  
+  invisible(all(loaded))
+}
+
+
+## ------------------------------------------------------------------
+## --- do data ingest work
+## ------------------------------------------------------------------
+dataingest <- function(data){
+
+  package.manager(c('dplyr', 'jsonlite'))
+
+  # ---- the class of each type
+  type <- sapply(data, class)
+
+  ## ---- create summary of numeric data
+  sum.number <- function(x, breaks = 31){
+    
+    h <- hist(x, plot = F, breaks = breaks)
+    h.l <- list(x = h$breaks, y = h$counts)
+    
+    t.df <- data.frame(x = as.matrix(summary(x)))
+    
+    l <- list(h.l, t.df)
+    
+    toJSON(l)
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
